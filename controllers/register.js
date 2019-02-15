63bf9262618bcdd9275de340791aa1f436b1ebcb
Amaraegbu Jedidiah <jedidiah.amaraegbu.182661@unn.edu.ng>
2019-02-15 13:01:19 +0100

    validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,38 +1,40 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const {name, email, password} = req.body
 
-  if(!name || !email || !password) {
-    return res.status(400).json('Incorrect form submission')
-  }
-  let hash = bcrypt.hashSync(password)
+  if(name === '' || email === '' || password === '') {
+    return res.status(400).json('All fields are required')
+  } else {
+    let hash = bcrypt.hashSync(password)
 
-  db.transaction(trx => {
-    trx.insert({
-      hash,
-      email
+    db.transaction(trx => {
+      trx.insert({
+        hash,
+        email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name,
+            email: loginEmail[0],
+            joined: new Date(),
+            following: 0,
+            followers: 0
+          })
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => {
+            res.status(400).json('An error occured<br />Try again')
+          }, trx.rollback)
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          name,
-          email: loginEmail[0],
-          joined: new Date(),
-          following: 0,
-          followers: 0
-        })
-        .then(user => {
-          res.json(user[0])
-        })
-        .catch(err => {
-          res.status(400).json('An error occured<br />Try again')
-        }, trx.rollback)
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
+  }
+
 }
 
 module.exports = {
