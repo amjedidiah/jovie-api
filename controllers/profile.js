1c4d42c0a420606dbeb1a0d754803e13ad5494e1
Amaraegbu Jedidiah <jedidiah.amaraegbu.182661@unn.edu.ng>
2019-02-15 13:07:55 +0100

    completed validation

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,37 +2,41 @@ const handleProfileGet = (req, res, db, bcrypt) => {
   const {name, email, phoneNumber, gender, birthday, password} = req.body
   const {id} = req.params
 
-  let hash = bcrypt.hashSync(password)
+  if(password === '') {
+    return res.status(400).json('Enter your password to edit your details')
+  } else {
+    let hash = bcrypt.hashSync(password)
 
-  db.transaction(trx => {
-    trx('login')
-    .where('id', '=', id)
-    .update({
-      hash,
-      email
+    db.transaction(trx => {
+      trx('login')
+      .where('id', '=', id)
+      .update({
+        hash,
+        email
+      })
+      .returning('email')
+      .then(upInfo => {
+        return trx('users')
+          .where('id', '=', id)
+          .returning('*')
+          .update({
+            name,
+            email,
+            phoneNumber,
+            gender,
+            birthday
+          })
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => {
+            res.status(400).json('try again')
+          }, trx.rollback)
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
-    .returning('email')
-    .then(upInfo => {
-      return trx('users')
-        .where('id', '=', id)
-        .returning('*')
-        .update({
-          name,
-          email,
-          phoneNumber,
-          gender,
-          birthday
-        })
-        .then(user => {
-          res.json(user[0])
-        })
-        .catch(err => {
-          res.status(400).json('try again')
-        }, trx.rollback)
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
+  }
 
 }
 
